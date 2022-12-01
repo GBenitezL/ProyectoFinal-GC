4ac0ef9f8d3056363d3ab73a641ee91afbfdb2c9
Gustavo Benitez <benitezlgustavo@gmail.com>
2022-11-30 21:08:22 -0600

    Intento Fix Ruedas

--- Assets/carController.cs
+++ Assets/carController.cs
@@ -1,95 +1,95 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public enum Axle{
-    Front, 
-    Back
-}
+// public enum Axle{
+//     Front, 
+//     Back
+// }
 
-public struct Wheel{
-    public GameObject model;
-    public WheelCollider wheelCollider;
-    public Axle axle;
-}
+// public struct Wheel{
+//     public GameObject model;
+//     public WheelCollider wheelCollider;
+//     public Axle axle;
+// }
 
 
-[RequireComponent(typeof(Rigidbody))]
+// [RequireComponent(typeof(Rigidbody))]
 
-public class carController : MonoBehaviour
-{
-    [SerializableField]
-    private float maxAcceleration = 20f;
-    [SerializableField]
-    private float turnSensitive = 1.0f,
-    [SerializableField]
-    private float maxAngle = 45.0f;
+// public class carController : MonoBehaviour
+// {
+//     [SerializableField]
+//     private float maxAcceleration = 20f;
+//     [SerializableField]
+//     private float turnSensitive = 1.0f,
+//     [SerializableField]
+//     private float maxAngle = 45.0f;
 
-    private float inputX, inputY;
+//     private float inputX, inputY;
     
-    private Rigidbody _rb;
+//     private Rigidbody _rb;
 
-    public List<AxleInfo> axleInfos = new List<AxleInfo>();
+//     public List<AxleInfo> axleInfos = new List<AxleInfo>();
 
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        _rb = GetComponent<Rigidbody>();
-    }
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         _rb = GetComponent<Rigidbody>();
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
+//     }
 
-    private void FixedUpdate(){
-        Move();
-    }
-    private void Move(){
-        foreach (AxleInfo info in axleInfos)
-        {
-            if(info.isBack)
-            {
-                info.rightWheel.motortorque = inputY * maxAcceleration * 500 * Time.deltaTime;
-                info.lefttWheel.motortorque = inputY * maxAcceleration * 500 * Time.deltaTime;
-            }
-            if(info.isFront)
-            {
-                var _steerAgle = inputX * turnSensitive * maxAngle;
-                info.rightWheel.steerAgle = Mathf.Lerp(info.rightWheel.steerAgle ,_steerAgle, 0.5f);
-                info.leftWheel.steerAgle = Mathf.Lerp(info.leftWheel.steerAgle , _steerAgle, 0.5f);
-            }
+//     private void FixedUpdate(){
+//         Move();
+//     }
+//     private void Move(){
+//         foreach (AxleInfo info in axleInfos)
+//         {
+//             if(info.isBack)
+//             {
+//                 info.rightWheel.motortorque = inputY * maxAcceleration * 500 * Time.deltaTime;
+//                 info.lefttWheel.motortorque = inputY * maxAcceleration * 500 * Time.deltaTime;
+//             }
+//             if(info.isFront)
+//             {
+//                 var _steerAgle = inputX * turnSensitive * maxAngle;
+//                 info.rightWheel.steerAgle = Mathf.Lerp(info.rightWheel.steerAgle ,_steerAgle, 0.5f);
+//                 info.leftWheel.steerAgle = Mathf.Lerp(info.leftWheel.steerAgle , _steerAgle, 0.5f);
+//             }
 
-            AnimeteWheels(info.rightWheel, info.visualRightWheel);
-            AnimeteWheels(info.leftWheel, info.visualleftWheel);
-        }
-    }
+//             AnimeteWheels(info.rightWheel, info.visualRightWheel);
+//             AnimeteWheels(info.leftWheel, info.visualleftWheel);
+//         }
+//     }
 
-    private void AnimeteWheels(WheelCollider wheelCollider, Transform wheelTransform)
-    {
-        Quaternion _rot;
-        Vector3 _pos;
+//     private void AnimeteWheels(WheelCollider wheelCollider, Transform wheelTransform)
+//     {
+//         Quaternion _rot;
+//         Vector3 _pos;
 
-        Vector3 rotate = Vector3.zero;
+//         Vector3 rotate = Vector3.zero;
 
-        wheelCollider.GetWorldPose(out _pos, out _rot);
-        wheelTransform.transform.rotation = _rot;
+//         wheelCollider.GetWorldPose(out _pos, out _rot);
+//         wheelTransform.transform.rotation = _rot;
 
-    }
-}
+//     }
+// }
 
-[System.Serializable]
+// [System.Serializable]
 
-public class AxleInfo(){
-    public WheelCollider rightWheel;
-    public WheelCollider leftWheel;
+// public class AxleInfo(){
+//     public WheelCollider rightWheel;
+//     public WheelCollider leftWheel;
 
-    public Transform visualRightWheel;
-    public Transform visualLeftWheel;
+//     public Transform visualRightWheel;
+//     public Transform visualLeftWheel;
 
-    public bool isBack;
-    public bool isFront;
-}
+//     public bool isBack;
+//     public bool isFront;
+// }
